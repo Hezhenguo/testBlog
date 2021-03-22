44860160f24e8c7da54b864471f4571063888bde
何振国 <hezg05@taikanglife.com>
2021-03-22 20:02:41 +0800

    test

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: '前端-Hezhenguo',
+  description: 'Personal Website',
+  themeConfig: {
+    search: false,
+    nav: [ // 导航栏配置
+      { text: '前端知识点总结', link: '/frontendBase/' },
+      { text: 'git相关', link: '/git/' },
+      { text: 'vue基础', link: '/vue/' }
+    ],
+    sidebar: 'auto',
+    sidebarDepth: 2 //侧边栏显示2级
+  }
+ 
+}
